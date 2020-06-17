3bb63ce15b9fcc2089be51b233b708746c655bc6
esargen <esargen@g.clemson.edu>
2020-06-16 20:23:26 -0400

    FIIIIIIINNNNNEEEEE also some js tweaks

--- events.js
+++ events.js
@@ -1,5 +1,30 @@
-const events = [
+/*import items from "./all.js";*/
+
+const items = [
   {
+    type: 'special',
+    business: 'Sushi 365',
+    offer: '$3.65 Sushi Rolls',
+    day: 'Monday',
+    bizlogo: 'https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/10474020_832999220074748_6215186670951263134_n.jpg?_nc_cat=100&_nc_sid=09cbfe&_nc_ohc=Kxu0B2IaBLcAX_kqw7b&_nc_ht=scontent-iad3-1.xx&oh=006fed725592c222e0d7d1e0fb2b9422&oe=5F0C99B1'
+  },
+  {
+    type: 'special',
+    business: 'Itsurwiener',
+    offer: '$6 Shipwrecks',
+    day: 'Friday',
+    time: '5-8pm',
+    bizlogo: 'https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/37322716_697868070553651_5167770909007675392_n.png?_nc_cat=111&_nc_sid=09cbfe&_nc_ohc=R9lr3Ff8KV0AX_oFf1p&_nc_ht=scontent-iad3-1.xx&oh=c06437832dea7d9df5282ece1e677ba0&oe=5F0CF2FF'
+  },
+  {
+    type: 'special',
+    business: 'BGR',
+    offer: '$6 Cheeseburger & Fries',
+    day: 'Wednesday',
+    bizlogo: 'https://www.tigerstogo.com/png/v_58_i101.png'
+  },
+  {
+    type: 'event',
     business: 'Sushi 365',
     reason: 'charity/nonprofit',
     bizevent: 'Eating Sushi off of Cheerleaders',
@@ -8,6 +33,7 @@ const events = [
     bizlogo: 'https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/10474020_832999220074748_6215186670951263134_n.jpg?_nc_cat=100&_nc_sid=09cbfe&_nc_ohc=Kxu0B2IaBLcAX_kqw7b&_nc_ht=scontent-iad3-1.xx&oh=006fed725592c222e0d7d1e0fb2b9422&oe=5F0C99B1'
   },
   {
+    type: 'event',
     business: 'Itsurwiener',
     reason: 'ladies\' night',
     bizevent: 'Wiener Rally',
@@ -16,6 +42,7 @@ const events = [
     bizlogo: 'https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/37322716_697868070553651_5167770909007675392_n.png?_nc_cat=111&_nc_sid=09cbfe&_nc_ohc=R9lr3Ff8KV0AX_oFf1p&_nc_ht=scontent-iad3-1.xx&oh=c06437832dea7d9df5282ece1e677ba0&oe=5F0CF2FF'
   },
   {
+    type: 'event',
     business: 'BGR',
     reason: 'family outing',
     bizevent: 'Pretending Burgers are Not Disgusting',
@@ -32,8 +59,9 @@ const time = document.querySelector('.time');
 
 const eventList = document.getElementById('category-list');
 
-for (i=0; i<events.length; i++) {
-  const item = events[i];
+for (i=0; i<items.length; i++) {
+  if (items[i].type === 'event') {
+  const item = items[i];
 
     newEvent = document.createElement("DIV");
     eventList.appendChild(newEvent);
@@ -42,7 +70,7 @@ for (i=0; i<events.length; i++) {
     if (item.hasOwnProperty('business')) {
       newAttr = document.createElement("P");
       newEvent.appendChild(newAttr);
-      newAttr.textContent = events[i].business;
+      newAttr.textContent = items[i].business;
       newAttr.classList += "business-name";
     }
 
@@ -53,7 +81,7 @@ for (i=0; i<events.length; i++) {
     if (item.hasOwnProperty('bizlogo')) {
       newImg = document.createElement("IMG");
       image.appendChild(newImg);
-      newImg.src = events[i].bizlogo;
+      newImg.src = items[i].bizlogo;
       newImg.classList += "bizlogo";
     }
 
@@ -64,7 +92,7 @@ for (i=0; i<events.length; i++) {
       if (item.hasOwnProperty('day')) {
         newAttr = document.createElement("P");
         dayTime.appendChild(newAttr);
-        newAttr.textContent = events[i].day;
+        newAttr.textContent = items[i].day;
         newAttr.classList += "day";
 
       }
@@ -72,7 +100,7 @@ for (i=0; i<events.length; i++) {
       if (item.hasOwnProperty('time')) {
         newAttr = document.createElement("P");
         dayTime.appendChild(newAttr);
-        newAttr.textContent = events[i].time;
+        newAttr.textContent = items[i].time;
         newAttr.classList += "time";
 
       }
@@ -84,17 +112,17 @@ for (i=0; i<events.length; i++) {
     if (item.hasOwnProperty('reason')) {
       newAttr = document.createElement("P");
       reasonevent.appendChild(newAttr);
-      newAttr.textContent = events[i].reason;
+      newAttr.textContent = items[i].reason;
       newAttr.classList += "reason";
     }
 
     if (item.hasOwnProperty('bizevent')) {
       newAttr = document.createElement("P");
       reasonevent.appendChild(newAttr);
-      newAttr.textContent = events[i].bizevent;
+      newAttr.textContent = items[i].bizevent;
       newAttr.classList += "bizevent";
 
     }
 
-
+}
 };
