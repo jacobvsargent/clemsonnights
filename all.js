890d99485bc9c6b53e90791e1a109d7d9a764404
jacobvsargent <jacobvsargent@gmail.com>
2020-06-21 15:07:11 -0400

    Update all.js with some actual events

--- all.js
+++ all.js
@@ -4,49 +4,62 @@ const items = [
     business: 'Sushi 365',
     offer: '$3.65 Sushi Rolls',
     day: 'Monday',
+    time: '5 pm - 10pm',
+    bizlogo: 'https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/10474020_832999220074748_6215186670951263134_n.jpg?_nc_cat=100&_nc_sid=09cbfe&_nc_ohc=Kxu0B2IaBLcAX_kqw7b&_nc_ht=scontent-iad3-1.xx&oh=006fed725592c222e0d7d1e0fb2b9422&oe=5F0C99B1'
+  },
+  {
+    type: 'special',
+    business: 'TDs',
+    offer: '$11.95 All You Can Eat Wings',
+    day: 'Monday',
+    time: '5 pm - 9pm',
+    bizlogo: 'https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/10474020_832999220074748_6215186670951263134_n.jpg?_nc_cat=100&_nc_sid=09cbfe&_nc_ohc=Kxu0B2IaBLcAX_kqw7b&_nc_ht=scontent-iad3-1.xx&oh=006fed725592c222e0d7d1e0fb2b9422&oe=5F0C99B1'
+  },
+  {
+    type: 'special',
+    business: 'Sushi 365',
+    offer: 'Half Price Wine Bottles',
+    day: 'Wednesday',
     bizlogo: 'https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/10474020_832999220074748_6215186670951263134_n.jpg?_nc_cat=100&_nc_sid=09cbfe&_nc_ohc=Kxu0B2IaBLcAX_kqw7b&_nc_ht=scontent-iad3-1.xx&oh=006fed725592c222e0d7d1e0fb2b9422&oe=5F0C99B1'
   },
   {
     type: 'special',
     business: 'Itsurwiener',
-    offer: '$6 Shipwrecks',
-    day: 'Friday',
-    time: '5-8pm',
+    offer: '$6 Litcher + Taco Bar',
+    day: 'Thursday',
+    time: '4-8pm',
     bizlogo: 'https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/37322716_697868070553651_5167770909007675392_n.png?_nc_cat=111&_nc_sid=09cbfe&_nc_ohc=R9lr3Ff8KV0AX_oFf1p&_nc_ht=scontent-iad3-1.xx&oh=c06437832dea7d9df5282ece1e677ba0&oe=5F0CF2FF'
   },
+  
+
+  /* DIVIDE BETWEEN SPECIALS AND EVENTS */
+
   {
-    type: 'special',
-    business: 'BGR',
-    offer: '$6 Cheeseburger & Fries',
-    day: 'Wednesday',
+    type: 'event',
+    business: 'TDs',
+    reason: 'karaoke',
+    bizevent: 'Karaoke @ TDs',
+    day: 'Tuesday',
+    time: '10 pm',
     bizlogo: 'https://www.tigerstogo.com/png/v_58_i101.png'
   },
   {
     type: 'event',
     business: 'Sushi 365',
-    reason: 'charity/nonprofit',
-    bizevent: 'Eating Sushi off of Cheerleaders',
-    day: 'Monday',
-    time: '8 pm - 2 am',
+    reason: 'trivia',
+    bizevent: 'Trivia Night (Pub Trivia)',
+    day: 'Wednesday',
+    time: '7:30 pm',
     bizlogo: 'https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/10474020_832999220074748_6215186670951263134_n.jpg?_nc_cat=100&_nc_sid=09cbfe&_nc_ohc=Kxu0B2IaBLcAX_kqw7b&_nc_ht=scontent-iad3-1.xx&oh=006fed725592c222e0d7d1e0fb2b9422&oe=5F0C99B1'
   },
   {
     type: 'event',
     business: 'Itsurwiener',
-    reason: 'ladies\' night',
-    bizevent: 'Wiener Rally',
-    day: 'Friday',
-    time: '5-6pm',
-    bizlogo: 'https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/37322716_697868070553651_5167770909007675392_n.png?_nc_cat=111&_nc_sid=09cbfe&_nc_ohc=R9lr3Ff8KV0AX_oFf1p&_nc_ht=scontent-iad3-1.xx&oh=c06437832dea7d9df5282ece1e677ba0&oe=5F0CF2FF'
-  },
-  {
-    type: 'event',
-    business: 'BGR',
-    reason: 'family outing',
-    bizevent: 'Pretending Burgers are Not Disgusting',
+    reason: 'trivia',
+    bizevent: 'Trivia Night (Themed Trivia)',
     day: 'Wednesday',
-    time: '8 am - 11 pm',
-    bizlogo: 'https://www.tigerstogo.com/png/v_58_i101.png'
+    time: '8 pm',
+    bizlogo: 'https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/37322716_697868070553651_5167770909007675392_n.png?_nc_cat=111&_nc_sid=09cbfe&_nc_ohc=R9lr3Ff8KV0AX_oFf1p&_nc_ht=scontent-iad3-1.xx&oh=c06437832dea7d9df5282ece1e677ba0&oe=5F0CF2FF'
   },
 ];
 
